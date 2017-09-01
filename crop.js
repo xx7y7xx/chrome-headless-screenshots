e3fb6ef23c2eff976b29ebbb3e7b781597cf46a0
Devin Chen <d3vin.chen@gmail.com>
2017-09-01 15:11:40 +0800

    crop all images

--- crop.js
+++ crop.js
@@ -1,8 +1,17 @@
-var Jimp = require("jimp");
+const Jimp = require("jimp");
+// const argv = require('minimist')(process.argv.slice(2));
+// const code = argv.code || 'Dupont';
 
-Jimp.read('output/Dupont.png', function (err, image) {
-  if (err) throw err;
-  image.autocrop(0.0003, false)
-    // .resize(256, 256)
-    .write("output/Dupont_new.jpg");
-});
+function crop(code) {
+  Jimp.read(`output/${code}.png`, function (err, image) {
+    if (err) throw err;
+    image.autocrop(0.0003, false)
+      // .resize(256, 256)
+      .write(`output/${code}_new.jpg`);
+  });
+}
+
+crop('Dupont');
+crop('ManagementStatus');
+crop('TrendAnalysis');
+crop('FinancialData');
