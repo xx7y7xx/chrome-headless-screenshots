d4232ccfde01c1727ae5a137b6e47108adda2f43
Devin Chen <d3vin.chen@gmail.com>
2017-09-21 10:56:28 +0800

    update template

--- passwd.tpl.js
+++ passwd.tpl.js
@@ -1,3 +1,13 @@
+// module.exports = [
+//  {
+//    username: 'foo',
+//    password: 'bar'
+//  },
+//  {
+//    username: 'foo2',
+//    password: 'bar2'
+//  }
+// ];
 module.exports = {
   username: 'foo',
   password: 'bar'
